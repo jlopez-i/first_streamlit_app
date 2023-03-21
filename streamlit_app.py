9b22a8c708df2add53bd9418d583c1fe9f4c838b
Juan López Iglesias <102589719+jlopez-i@users.noreply.github.com>
2023-03-21 15:38:54 +0100

    Update streamlit_app.py
    Some errors solved

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-setreamlit.text('Omega 3 & Blueberry Oatmeal')
-setreamlit.text('Kale, Spinach & Rocket Smoothie')
-setreamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
