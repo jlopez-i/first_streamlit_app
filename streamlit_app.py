a9f1d4635e23e8d032b0ddedcc812411b12c3d0d
Juan López Iglesias <102589719+jlopez-i@users.noreply.github.com>
2023-03-21 16:04:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,14 +15,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #called read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#We want the customer to be able to choose the fruits by name!!
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-#The picker works, but the numbers don't make any sense! 
-#We want the customer to be able to choose the fruits by name!!
-
-my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #After pulling the data into a pandas dataframe called my_fruit_list, 
 #we will ask the streamlit library to display it on the page by typing:
