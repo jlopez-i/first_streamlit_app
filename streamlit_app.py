4025e8ef038fb6612320df03fca194a585131b1d
Juan López Iglesias <102589719+jlopez-i@users.noreply.github.com>
2023-03-21 15:37:13 +0100

    Update streamlit_app.py
    added a header and text description about meals

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+setreamlit.text('Omega 3 & Blueberry Oatmeal')
+setreamlit.text('Kale, Spinach & Rocket Smoothie')
+setreamlit.text('Hard-Boiled Free-Range Egg')
