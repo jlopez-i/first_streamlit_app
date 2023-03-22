cc86cbe5e3adab8a025d3b4e5dce244a5bb85425
Juan López Iglesias <102589719+jlopez-i@users.noreply.github.com>
2023-03-22 18:54:45 +0100

    Update streamlit_app.py
    added    my_cnx.close()
    change title label
    change button label

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ except URLError as e:
 # Let's Query Our Trial Account Metadata (commented)
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 # Snnowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -72,9 +72,10 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows) 
     
 # Allow to the end user to add a fruit to the list
