c43cd47cc522ec380f5aa72f04e8429f876c62a0
Juan López Iglesias <102589719+jlopez-i@users.noreply.github.com>
2023-03-22 17:22:59 +0100

    Update streamlit_app.py
     Let's Query Our Trial Account Metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import libraries
 import streamlit
 import pandas
-import snowflake.connector
+
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -50,3 +50,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # Display it on the page as a Table
 streamlit.dataframe(fruityvice_normalized)
+
+#  Let's Query Our Trial Account Metadata 
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
